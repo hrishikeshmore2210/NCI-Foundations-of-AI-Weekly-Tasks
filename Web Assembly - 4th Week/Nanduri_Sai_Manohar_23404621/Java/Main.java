022c6436a0f9397d4cc6d2a90ac48fab32d6e7d9
Nanduri Sai Manohar <143052842+NSM1997@users.noreply.github.com>
2025-02-19 17:21:57 +0000

    Add files via upload

---
+++ Web Assembly - 4th Week/Nanduri_Sai_Manohar_23404621/Java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr ={10,20,30};
+        for(int i=0;i<arr.length;i++){
+            System.out.println("arr["+i+"]:"+ arr[i]);
+        }
+    } 
+}
---
+++ Web Assembly - 4th Week/Nanduri_Sai_Manohar_23404621/Python/intro.py
@@ -0,0 +1,8 @@
+def myFunc():
+    my_list = [10,20,30]
+    for i in range(len(my_list)):
+        print(f'my_list{[i]}:',my_list[i])
+
+myFunc()
+
+# print("Hello from WebAssembly-Pyodide!")
